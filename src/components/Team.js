7f6e3ddff4e534ee73df06334006c05cb52eacf2
SanJorge <jevaldiviah@gmail.com>
2020-09-21 11:41:57 -0400

    PreBuild

---
+++ src/components/Team.js
@@ -0,0 +1,273 @@
+import React from "react";
+import {
+    MDBIcon,
+    MDBRow,
+    MDBCol,
+    MDBMask,
+    MDBView
+} from "mdbreact";
+
+const Team = () => {
+    return (
+        <div>
+            <div className="divider-new">
+                <h2 className='h1-responsive blue-text font-weight-bold text-center'>Choose your character</h2>
+            </div>
+
+            <MDBRow>
+                <MDBCol size="md">
+                    <div  size="md" className="z-depth-2 mb-3 rounded text-center d-flex justify-content-start" bgColor="white">
+                        <img src={require("../images/Face.jpg")}
+                             className="img-fluid rounded-left"
+                             width="120" height="120"
+                             alt="" />
+                        <div className="container-fluid p-2">
+                            <h5 className='h4-responsive font-weight-bold text-center'>SanJorge</h5>
+                            <p>"El Crema" and you know it</p>
+                            <div className='text-center'>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='facebook-f' size="lg" className="blue-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='twitter' size="lg" className="blue-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='github' size="lg" className="dark-grey-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='linkedin' size="lg" className="blue-text"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </MDBCol>
+
+                <MDBCol size="md">
+                    <div  size="md" className="z-depth-2 mb-3 rounded text-center d-flex justify-content-start" bgColor="white">
+                        <img src={require("../images/Nosotros/Jito.jpg")}
+                             className="img-fluid rounded-left"
+                             width="120" height="120"
+                             alt="" />
+                        <div className="container-fluid p-2">
+                            <h5 className='h4-responsive font-weight-bold text-center'>Jorgito</h5>
+                            <p>"The Shank of punishment"</p>
+                            <div className='text-center'>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='facebook-f' size="lg" className="blue-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='twitter' size="lg" className="blue-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='github' size="lg" className="dark-grey-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='linkedin' size="lg" className="blue-text"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </MDBCol>
+
+                <MDBCol size="md">
+                    <div  size="md" className="z-depth-2 mb-3 rounded text-center d-flex justify-content-start" bgColor="white">
+                        <img src={require("../images/Nosotros/Konami.jpg")}
+                             className="img-fluid rounded-left"
+                             width="120" height="120"
+                             alt="" />
+                        <div className="container-fluid p-2">
+                            <h5 className='h4-responsive font-weight-bold text-center'>Konami</h5>
+                            <p>"Wreaking Ball"</p>
+                            <div className='text-center'>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='facebook-f' size="lg" className="blue-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='twitter' size="lg" className="blue-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='github' size="lg" className="dark-grey-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='linkedin' size="lg" className="blue-text"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </MDBCol>
+            </MDBRow>
+
+            <MDBRow>
+                <MDBCol size="md">
+                    <div  size="md" className="z-depth-2 mb-3 rounded text-center d-flex justify-content-start" bgColor="white">
+                        <img src={require("../images/Nosotros/Roberto.jpg")}
+                             className="img-fluid rounded-left"
+                             width="120" height="120"
+                             alt="" />
+                        <div className="container-fluid p-2">
+                            <h5 className='h4-responsive font-weight-bold text-center'>Kirito</h5>
+                            <p>"Aceggrrre ya"</p>
+                            <div className='text-center'>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='facebook-f' size="lg" className="blue-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='twitter' size="lg" className="blue-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='github' size="lg" className="dark-grey-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='linkedin' size="lg" className="blue-text"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </MDBCol>
+
+                <MDBCol size="md">
+                    <div  size="md" className="z-depth-2 mb-3 rounded text-center d-flex justify-content-start" bgColor="white">
+                        <img src={require("../images/Nosotros/Paco.jpg")}
+                             className="img-fluid rounded-left"
+                             width="120" height="120"
+                             alt="" />
+                        <div className="container-fluid p-2">
+                            <h5 className='h4-responsive font-weight-bold text-center'>Paco</h5>
+                            <p>"The invisible man" (in many ways)</p>
+                            <div className='text-center'>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='facebook-f' size="lg" className="blue-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='twitter' size="lg" className="blue-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='github' size="lg" className="dark-grey-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='linkedin' size="lg" className="blue-text"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </MDBCol>
+
+                <MDBCol size="md">
+                    <div  size="md" className="z-depth-2 mb-3 rounded text-center d-flex justify-content-start" bgColor="white">
+                        <img src={require("../images/Nosotros/DarkLink.jpg")}
+                             className="img-fluid rounded-left"
+                             width="120" height="120"
+                             alt="" />
+                        <div className="container-fluid p-2">
+                            <h5 className='h4-responsive font-weight-bold text-center'>Dark Link</h5>
+                            <p>"A man of action"</p>
+                            <div className='text-center'>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='facebook-f' size="lg" className="blue-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='twitter' size="lg" className="blue-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='github' size="lg" className="dark-grey-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='linkedin' size="lg" className="blue-text"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </MDBCol>
+            </MDBRow>
+
+            <MDBRow>
+                <MDBCol size="md">
+                    <div  size="md" className="z-depth-2 mb-3 rounded text-center d-flex justify-content-start" bgColor="white">
+                        <img src={require("../images/Nosotros/Arthuro.jpg")}
+                             className="img-fluid rounded-left"
+                             width="120" height="120"
+                             alt="" />
+                        <div className="container-fluid p-2">
+                            <h5 className='h4-responsive font-weight-bold text-center'>Arturo</h5>
+                            <p>"A man whitout a filter"</p>
+                            <div className='text-center'>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='facebook-f' size="lg" className="blue-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='twitter' size="lg" className="blue-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='github' size="lg" className="dark-grey-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='linkedin' size="lg" className="blue-text"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </MDBCol>
+
+                <MDBCol size="md">
+                    <div  size="md" className="z-depth-2 mb-3 rounded text-center d-flex justify-content-start" bgColor="white">
+                        <img src={require("../images/Nosotros/Alike.jpg")}
+                             className="img-fluid rounded-left"
+                             width="120" height="120"
+                             alt="" />
+                        <div className="container-fluid p-2">
+                            <h5 className='h4-responsive font-weight-bold text-center'>Alike</h5>
+                            <p>"The Nurse"</p>
+                            <div className='text-center'>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='facebook-f' size="lg" className="blue-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='twitter' size="lg" className="blue-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='github' size="lg" className="dark-grey-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='linkedin' size="lg" className="blue-text"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </MDBCol>
+
+                <MDBCol size="md">
+                    <div  size="md" className="z-depth-2 mb-3 rounded text-center d-flex justify-content-start" bgColor="white">
+                        <MDBView className="rounded-left">
+                        <img src={require("../images/Nosotros/Jesus.png")}
+                             className="rounded-left"
+                             width="120" height="120"
+                             alt="" />
+                        <MDBMask className="flex-center" overlay="black-strong" />
+                        </MDBView>
+                        <div className="container-fluid p-2">
+                            <h5 className='h4-responsive font-weight-bold text-center'>Jesus</h5>
+                            <p>This player is not longer available</p>
+                            <div className='text-center'>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='facebook-f' size="lg" className="blue-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='twitter' size="lg" className="blue-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='github' size="lg" className="dark-grey-text"/>
+                                </a>
+                                <a href="" className="mx-2">
+                                    <MDBIcon fab icon='linkedin' size="lg" className="blue-text"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </MDBCol>
+            </MDBRow>
+
+        </div>
+    );
+}
+
+export default Team;
