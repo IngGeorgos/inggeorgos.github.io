8429f6258f12661aef0cb58cc9a0324f83eced36
SanJorge <jevaldiviah@gmail.com>
2020-09-24 16:09:49 -0400

    PreBuild

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a8a09e374933688647e858fff3741765.js"
+  "/precache-manifest.e2998af0dc82bdbde188d99acab6017c.js"
 );
 
 self.addEventListener('message', (event) => {
