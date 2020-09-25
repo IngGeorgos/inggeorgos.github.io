205a4c2da78bd75bda9683acf13d035f09c9b7cb
SanJorge <jevaldiviah@gmail.com>
2020-09-25 19:25:01 -0400

    PreBuild

--- !PushBuild.bat
+++
@@ -1,7 +0,0 @@
-cd C:\xampp\htdocs\MirageTech
-git init
-git add .
-git commit -m "PreBuild"
-git branch -M master
-git remote add origin https://github.com/IngGeorgos/inggeorgos.github.io.git
-git push -u origin master
