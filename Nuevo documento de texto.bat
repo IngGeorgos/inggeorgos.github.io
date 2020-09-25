250d7d3bdf40e7fdc6c5795cd6ffbd130ea59e9f
SanJorge <jevaldiviah@gmail.com>
2020-09-25 19:20:10 -0400

    Build

---
+++ Nuevo documento de texto.bat
@@ -0,0 +1,7 @@
+cd C:\xampp\htdocs\MirageTech
+git init
+git add .
+git commit -m "Build"
+git branch -M gh-pages
+git remote add origin https://github.com/IngGeorgos/inggeorgos.github.io.git
+git push -u origin gh-pages
